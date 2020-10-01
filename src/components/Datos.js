cf8347d4513357c8ab79369958b36ff2337a26bf
Raul T <jr.torsa@gmail.com>
2020-10-01 16:35:36 -0600

    first steps

--- src/components/Datos.js
+++ src/components/Datos.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { Vacations } from "../utils/vacations";
+
+console.log(Vacations);
 
 const Datos = () => {
   return (
     <div>
-      <p>desde datos</p>
+      <form>
+        <div>
+          <label>Salario Diario</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Fecha de Ingreso</label>
+          <input type="date" />
+        </div>
+        <div>
+          <label>Fecha de Egreso</label>
+          <input type="date" />
+        </div>
+        <div>
+          <label>Vacaciones Correspondientes</label>
+          <select>
+            <option>-- Seleccione un Periodo --</option>
+            {Vacations.map((time) => (
+              <option key={time.id}>{time.period}</option>
+            ))}
+          </select>
+        </div>
+      </form>
     </div>
   );
 };
