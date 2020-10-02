5a3d67200625b66fe19c80b0e015765bb04fe035
Raul Torres <jr.torsa@gmail.com>
2020-10-01 21:41:36 -0600

    adding time periods

--- src/components/Datos.js
+++ src/components/Datos.js
@@ -1,30 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import { Vacations } from "../utils/vacations";
 
-console.log(Vacations);
 
 const Datos = () => {
+
+
+  const [data, setData] = useState({
+    salary: '',
+    firstDate: '',
+    secondDate: '',
+    timeFrame: ''
+  }) 
+
+  const { salary, firstDate, secondDate, timeFrame } = data
+
+  const handleChange = e => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value      
+    })
+  }
+
   return (
     <div>
       <form>
         <div>
           <label>Salario Diario</label>
-          <input type="text" />
+          <input type="text" name='salary' value={salary} onChange={handleChange} />
         </div>
         <div>
           <label>Fecha de Ingreso</label>
-          <input type="date" />
+          <input type="date" name='firstDate' value={firstDate} onChange={handleChange} />
         </div>
         <div>
           <label>Fecha de Egreso</label>
-          <input type="date" />
+          <input type="date" name='secondDate' value={secondDate} onChange={handleChange} />
         </div>
         <div>
-          <label>Vacaciones Correspondientes</label>
-          <select>
-            <option>-- Seleccione un Periodo --</option>
+          <label>Anos Laborados</label>
+          <select onChange={handleChange} name='timeFrame' value={timeFrame}>
+            <option value=''>-- Seleccione un Periodo --</option>
             {Vacations.map((time) => (
-              <option key={time.id}>{time.id}</option>
+              <option key={time.id}  value={time.period}>{time.id}</option>
             ))}
           </select>
         </div>
