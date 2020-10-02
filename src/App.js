c36984b98bd4ca2f59d94891ce8cdf4e01b2e3d2
Raul Torres <jr.torsa@gmail.com>
2020-10-01 22:07:55 -0600

    changing styles

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Datos from "./components/Datos";
 
 function App() {
   return (
-    <div>
-      <h1 style={{textAlign:'center'}}>Calculadora Laboral</h1>
+    <div className='container'>
+            <h1 style={{textAlign:'center'}}>Calculadora Laboral</h1>
+
+      <div className='row'>
+        <div className='one-half column'>
       <Datos />
+        </div>
+        <div className='one-half column'>2</div>
+      </div>
     </div>
   );
 }
