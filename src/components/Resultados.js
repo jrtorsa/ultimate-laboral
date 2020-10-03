78b57c506c98fd6f1133c5aee0997ffe7c20a241
Raul Torres <jr.torsa@gmail.com>
2020-10-03 00:19:14 -0600

    making first calculation

--- src/components/Resultados.js
+++ src/components/Resultados.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const Resultados = ({ nuevoCalculo }) => {
 
+  const nihtyDays = nuevoCalculo.salary * 90
+
   return (
     <div>
       <div>
-        <p>90 dias: {nuevoCalculo.salary}</p>
+        <p>90 dias: {nihtyDays}</p>
       </div>
     </div>
   );
