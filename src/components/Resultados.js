8aecf4e896b3d6384adcb5e4d2673816cb270f9f
Raul T <jr.torsa@gmail.com>
2020-10-03 14:37:20 -0600

    fixing vacation days

--- src/components/Resultados.js
+++ src/components/Resultados.js
@@ -3,13 +3,19 @@ import React from "react";
 const Resultados = ({ nuevoCalculo }) => {
   const dailyIntegratedSalary =
     parseInt(nuevoCalculo.salary) + parseInt(nuevoCalculo.bonos);
-  const nightyDays = nuevoCalculo.salary * 90;
+  console.log(nuevoCalculo.timeFrame);
+  //const years = parseInt(nuevoCalculo.timeFrame);
+
+  const nightyDays = dailyIntegratedSalary * 90;
+
+  //const twentyDays = dailyIntegratedSalary * 20 * years;
 
   return (
     <div>
       <div>
         <p>Salario Diario Integrado: ${dailyIntegratedSalary}</p>
         <p>90 dias: ${nightyDays}</p>
+        {/* <p>20 dias: ${twentyDays}</p> */}
       </div>
     </div>
   );
